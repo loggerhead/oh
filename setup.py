add27c11ff492829fc4fb0f52afe010ca1f81bc3
loggerhead <lloggerhead@gmail.com>
2017-01-06 17:10:54 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='oh',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple TCP/UDP echo server',
     author='loggerhead',
     author_email='lloggerhead@gmail.com',
